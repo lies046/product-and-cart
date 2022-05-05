b9c5dd86bdd214e6175c99539db5045e6cf81144
Shohei Ito <itou@bis.ne.jp>
2022-05-05 15:22:59 +0900

    ファイル追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductsView from '../views/ProductsView.vue'
+import PastOrdersView from '../views/PastOrdersView.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/products',
     name: 'products',
     component: ProductsView
+  },
+  {
+    path: '/past-orders',
+    name: 'PastOrders',
+    component: PastOrdersView
   }
 ]
 
